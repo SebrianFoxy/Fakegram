8ac1d8a9d2e150514e04f0b6cd67eca90dad1355
kimrenny <sevdon4@gmail.com>
2025-08-27 18:14:55 +0300

    fix: improve DB initialization and reorganize backend folder structure

--- backend/internal/database/init_db.go
+++ backend/internal/database/init_db.go
@@ -1,18 +1,37 @@
 package database
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "fakegram-api/internal/config"
+	"database/sql"
+	"fakegram-api/internal/config"
+	"fmt"
+	"log"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func InitDB(cfg *config.Config) (*sql.DB, error) {
+    adminConn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=postgres sslmode=disable",
+        cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword)
+    dbAdmin, err := sql.Open("postgres", adminConn)
+    if err != nil {
+        return nil, err
+    }
+    defer dbAdmin.Close()
+
+    var exists int
+    err = dbAdmin.QueryRow("SELECT 1 FROM pg_database WHERE datname=$1", cfg.DBName).Scan(&exists)
+    if err == sql.ErrNoRows {
+        _, err = dbAdmin.Exec("CREATE DATABASE " + cfg.DBName)
+        if err != nil {
+            return nil, fmt.Errorf("failed to create database: %v", err)
+        }
+        log.Printf("Database %s created successfully", cfg.DBName)
+    } else if err != nil {
+        return nil, err
+    }
+
     connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
         cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
-
     db, err := sql.Open("postgres", connStr)
     if err != nil {
         return nil, err
@@ -26,6 +45,7 @@ func InitDB(cfg *config.Config) (*sql.DB, error) {
     log.Println("Successfully connected to PostgreSQL!")
     return db, nil
 }
+
 
 func CreateTableUsers(db *sql.DB) error {
     query := `
