0180384cc57aba4df0c1ea17bff2f4d5e59ff6a1
kimrenny <sevdon4@gmail.com>
2025-09-02 19:03:27 +0300

    refactor: move file to separate folder for improved project structure

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:fakegram/presenter/messages/page/profile.dart';
-
+import 'package:fakegram/presenter/profile/page/profile.dart';
 import 'presenter/messages/page/messages.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
